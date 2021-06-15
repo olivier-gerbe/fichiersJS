c67078c573b51617088cd95f8e561c4656d30085
oliviergerbe <oliviergerbe@imac-de-bk>
2021-06-15 09:33:55 -0400

    update

--- rewrite_url.js
+++ rewrite_url.js
@@ -1,11 +1,11 @@
 function rewriteURL (portfoliocode) {
-	if (portfoliocode.indexOf("eportfolium-com")>-1 && window.location.pathname.indexOf("index.htm")<0){
-		history.pushState('', '', '../../index.htm');
+	if (portfoliocode.indexOf("eportfolium-com")>-1){
+		history.pushState('', '', 'cooperative.htm');
 	}
 	if (portfoliocode.indexOf("site-karuta-project")>-1 && window.location.pathname.indexOf("index.htm")<0){
-		history.pushState('', '', '../../index.htm');
+		history.pushState('', '', 'project.htm');
 	}
 	if (portfoliocode.indexOf("documentation-karuta")>-1 && window.location.pathname.indexOf("index.htm")<0){
-		history.pushState('', '', '../../index.htm');
+		history.pushState('', '', 'documentation.htm');
 	}
 }
