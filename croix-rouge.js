b6abfc108649b02b7eae6c1506f5e70a7c59bf33
olivier.gerbe <olivier.gerbe@hec.ca>
2024-11-03 11:38:12 -0500

    croix-rouge

---
+++ croix-rouge.js
@@ -0,0 +1,123 @@
+
+
+//==================================
+function messageLu(nodeid)
+//==================================
+{
+	viewed = false;
+	let itself = UICom.structure.ui[nodeid].node;
+	// -----------------------------
+	// search if already viewded by the USER
+	const views = $("asmContext:has(metadata[semantictag*='view-by'])",itself);
+	for (i=0;i<views.length;i++){
+		const viewid =  $(views[i]).attr("id");
+		const viewtext = $(UICom.structure.ui[viewid].resource.text_node[LANGCODE]).text();
+		if (viewtext.indexOf(USER.firstname)>-1 && viewtext.indexOf(USER.lastname)>-1) {
+			viewed = true;
+			break;
+		}
+	}
+	//-----------------------------
+	if (!viewed) {
+		//--------------------------
+		// import et mise à jour de 'view-by'
+		const srcecode = replaceVariable("##dossier-modeles-LAN##.composantes-generales")
+		const viewid = importBranch(nodeid,srcecode,"view-by");
+		UIFactory.Node.reloadUnit(nodeid,false);
+		const today = new Date();
+		let  viewtext =today.toLocaleString() + " par " + USER.firstname + " "+ USER.lastname;
+		if (views.length==0) 
+			viewtext = "<span style='display:none'>Créé le " + viewtext +" </span>";
+		else
+			viewtext =  "Vu le " + viewtext;
+		UICom.structure.ui[viewid].resource.text_node[LANGCODE].text(viewtext);
+		UICom.structure.ui[viewid].save();
+		if (views.length==0) 
+			moveup(viewid);
+		UIFactory.Node.reloadUnit();
+		UIFactory.Node.reloadUnit();
+	}
+}
+
+//==================================
+function testSiEnvoiCourrielCR(nodeid)
+//==================================
+{
+	let result = true;
+	//-------------------------------
+	const parent = UICom.structure.ui[nodeid].node;
+	const notified = $("asmContext:has(metadata[semantictag*='notified-on'])",parent);
+	const notifiedid =  $(notified).attr("id");
+	if (UICom.structure.ui[notifiedid].resource.text_node[LANGCODE].text()!='')
+		result = false;	
+	return result;
+}
+
+//==================================
+function envoiEmails(message,emails) 
+//==================================
+{
+	var elt = document.createElement("p");
+	elt.textContent = message;
+	message = elt.innerHTML;
+	message = message.replace(/..\/..\/..\/..\/..\/../g, window.location.protocol+"//"+window.location.host);
+	//------------------------------
+	for (i=0;i<emails.length;i++){
+		var xml ="<node>";
+		xml +="<sender>"+$(USER.email_node).text()+"</sender>";
+		xml +="<recipient>"+emails[i]+"</recipient>";
+		xml +="<subject>"+USER.firstname+" "+USER.lastname+" a posté un message.</subject>";
+		xml +="<message>"+message+"</message>";
+		xml +="<recipient_cc></recipient_cc><recipient_bcc></recipient_bcc>";
+		xml +="</node>";
+		$.ajax({
+			contentType: "application/xml",
+			type : "POST",
+			dataType : "xml",
+			url : "../../../"+serverBCK+"/mail",
+			data: xml,
+			success : function(data) {
+				$('#edit-window').modal('hide');
+			}
+		});
+	}
+	let notification = "Un courriel a été envoyé à <ul> ";
+	for (i=0;i<emails.length;i++){
+		notification += "<li>" + emails[i] + "</li>";
+	}	
+	notification += "</ul>"
+	alertHTML(notification);
+}
+
+//==================================
+function envoiCourrielCR(nodeid)
+//==================================
+{
+	const today = new Date();
+	//-------------------------------
+	const parent = UICom.structure.ui[nodeid].node;
+	const messtextnode = $("asmContext:has(metadata[semantictag*='texte-commentaire'])",parent);
+	const messtextnodeid =  $(messtextnode).attr("id");
+	const messtext = UICom.structure.ui[messtextnodeid].resource.text_node[LANGCODE].text();
+	//-------------------------------
+	let message = "";
+	message += "Bonjour,<br><br>";
+	message += USER.firstname + " " + USER.lastname +" a posté un message  le "+today.toLocaleString()+"<br><br>";
+	message += "<div style='padding:5px;border:1px solid #c6c2c1'>" + messtext + "</div>"
+	const courriels = $("asmContext:has(metadata[semantictag*='email-'])",g_portfolio_current);
+	var emails = [];
+	for (i=0;i<courriels.length;i++){
+		const courrielid =  $(courriels[i]).attr("id");
+		const email = UICom.structure.ui[courrielid].resource.text_node[LANGCODE].text()
+		if (email.length>0 && email.indexOf(USER.email)<0)
+			emails.push(email);
+	}
+	envoiEmails(message,emails) ;
+	submit(nodeid);
+	UIFactory.Node.reloadUnit();
+}
+
+//# sourceURL=croix-rouge.js
+
+
+
